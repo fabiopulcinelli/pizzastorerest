84fbc8094b5040ac062ec2ca8a7e13e9d20a481f
unknown <fabiopulcinelli2k2@gmail.com>
2022-12-26 14:33:36 +0100

    primo commit

---
+++ src/main/java/it/prova/pizzastorerest/PizzastorerestApplication.java
@@ -0,0 +1,20 @@
+package it.prova.pizzastorerest;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PizzastorerestApplication implements CommandLineRunner{
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PizzastorerestApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
+
+}
